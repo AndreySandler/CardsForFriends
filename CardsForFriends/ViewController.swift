bfcbd6f08f70049ccbfdccf414f3f0f7541f3dcb
AndreySandler <mrdreem13@gmail.com>
2022-10-04 22:57:43 +0300

    Initial Commit

---
+++ CardsForFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardsForFriends
+//
+//  Created by Andrey Sandler on 04.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
