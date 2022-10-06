e934874ab662b8dffe052dd63e9ceda110278d78
AndreySandler <mrdreem13@gmail.com>
2022-10-06 12:09:54 +0300

    Gradient code fix

--- CardsForFriends/ViewControllers/CardViewController.swift
+++ CardsForFriends/ViewControllers/CardViewController.swift
@@ -28,11 +28,7 @@ class CardViewController: UIViewController {
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.frame = view.bounds
-        gradientLayer.colors = [#colorLiteral(red: 0.9507465959, green: 0.480499506, blue: 0.507527709, alpha: 1).cgColor, #colorLiteral(red: 0.866619885, green: 0.6335844994, blue: 0.9783375859, alpha: 1).cgColor]
-        view.layer.insertSublayer(gradientLayer, at: 0)
+        addGradient()
         
         nameLabel.text = name
         surnameLabel.text = surname
--- CardsForFriends/ViewControllers/HomeViewController.swift
+++ CardsForFriends/ViewControllers/HomeViewController.swift
@@ -15,14 +15,20 @@ class HomeViewController: UIViewController {
     // MARK: - Public Properties
     var greeting: String!
     
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addGradient()
+        greetingLabel.text = "Welcome, \(greeting ?? "")!"
+    }
+}
+
+extension UIViewController {
+    func addGradient() {
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
         gradientLayer.colors = [#colorLiteral(red: 0.9507465959, green: 0.480499506, blue: 0.507527709, alpha: 1).cgColor, #colorLiteral(red: 0.866619885, green: 0.6335844994, blue: 0.9783375859, alpha: 1).cgColor]
         view.layer.insertSublayer(gradientLayer, at: 0)
-        
-        greetingLabel.text = "Welcome, \(greeting ?? "")!"
     }
 }
